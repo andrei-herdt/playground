39874e597b113bb249d483684c34db5bd526ae06
Andrei Herdt <andrei.herdt@arrival.com>
2023-08-03 00:09:40 +0100

    id

--- src/viewer_example.py
+++ src/viewer_example.py
@@ -3,22 +3,24 @@ import time
 import mujoco
 import mujoco.viewer
 
-import numpy as np
+import helpers
 
 model = mujoco.MjModel.from_xml_path(
     '/workdir/mujoco/model/humanoid/humanoid.xml')
 data = mujoco.MjData(model)
 
+mujoco.mj_resetDataKeyframe(model, data, 1)
 with mujoco.viewer.launch_passive(model, data) as viewer:
     # Close the viewer automatically after 30 wall-seconds.
     start = time.time()
     while viewer.is_running() and time.time() - start < 30:
         step_start = time.time()
 
-        # Set control vector.
-        data.ctrl = np.random.randn(model.nu)
-        # mj_step can be replaced with code that also evaluates
-        # a policy and applies a control signal before stepping the physics.
+        data.qacc = 0  # Assert that there is no the acceleration.
+        mujoco.mj_inverse(model, data)
+        print(data.qfrc_inverse)
+
+        input("step")
         mujoco.mj_step(model, data)
 
         # Example modification of a viewer option: toggle contact points every two seconds.
