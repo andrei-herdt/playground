96a31116eb2781bfc1ce16f0ef1232d30bd78d93
Andrei Herdt <andrei.herdt@arrival.com>
2023-08-03 21:02:35 +0100

    add LQR

--- src/viewer_example.py
+++ src/viewer_example.py
@@ -4,29 +4,146 @@ import mujoco
 import mujoco.viewer
 
 import helpers
+import matplotlib.pyplot as plt
+import numpy as np
+
+import scipy
 
 model = mujoco.MjModel.from_xml_path(
     '/workdir/mujoco/model/humanoid/humanoid.xml')
 data = mujoco.MjData(model)
 
+height_offsets = np.linspace(-0.001, 0.001, 2001)
+vertical_forces = []
+for offset in height_offsets:
+    mujoco.mj_resetDataKeyframe(model, data, 1)
+    mujoco.mj_forward(model, data)
+    data.qacc = 0
+    # Offset the height by `offset`.
+    data.qpos[2] += offset
+    mujoco.mj_inverse(model, data)
+    vertical_forces.append(data.qfrc_inverse[2])
+
+# Find the height-offset at which the vertical force is smallest.
+idx = np.argmin(np.abs(vertical_forces))
+best_offset = height_offsets[idx]
+
+
 mujoco.mj_resetDataKeyframe(model, data, 1)
+mujoco.mj_forward(model, data)
+data.qacc = 0
+data.qpos[2] += best_offset
+qpos0 = data.qpos.copy()  # Save the position setpoint.
+mujoco.mj_inverse(model, data)
+qfrc0 = data.qfrc_inverse.copy()
+print('desired forces: \n', qfrc0)
+
+ctrl0 = np.atleast_2d(qfrc0) @ np.linalg.pinv(data.actuator_moment)
+ctrl0 = ctrl0.flatten()  # Save the ctrl setpoint.
+print('control setpoint: \n', ctrl0)
+
+data.ctrl = ctrl0
+mujoco.mj_forward(model, data)
+print('actuator forces: \n', data.qfrc_actuator)
+
+nu = model.nu  # Alias for the number of actuators.
+R = np.eye(nu)
+
+nv = model.nv  # Shortcut for the number of DoFs.
+
+# Get the Jacobian for the root body (torso) CoM.
+mujoco.mj_resetData(model, data)
+data.qpos = qpos0
+mujoco.mj_forward(model, data)
+jac_com = np.zeros((3, nv))
+mujoco.mj_jacSubtreeCom(model, data, jac_com, model.body('torso').id)
+
+# Get the Jacobian for the left foot.
+jac_foot = np.zeros((3, nv))
+mujoco.mj_jacBodyCom(model, data, jac_foot, None, model.body('foot_left').id)
+ctrl0 = np.atleast_2d(qfrc0) @ np.linalg.pinv(data.actuator_moment)
+ctrl0 = ctrl0.flatten()  # Save the ctrl setpoint.
+jac_diff = jac_com - jac_foot
+Qbalance = jac_diff.T @ jac_diff
+
+# Get all joint names.
+joint_names = [model.joint(i).name for i in range(model.njnt)]
+
+# Get indices into relevant sets of joints.
+root_dofs = range(6)
+body_dofs = range(6, nv)
+abdomen_dofs = [
+    model.joint(name).dofadr[0]
+    for name in joint_names
+    if 'abdomen' in name
+    and not 'z' in name
+]
+left_leg_dofs = [
+    model.joint(name).dofadr[0]
+    for name in joint_names
+    if 'left' in name
+    and ('hip' in name or 'knee' in name or 'ankle' in name)
+    and not 'z' in name
+]
+balance_dofs = abdomen_dofs + left_leg_dofs
+other_dofs = np.setdiff1d(body_dofs, balance_dofs)
+
+# Cost coefficients.
+BALANCE_COST = 1000  # Balancing.
+BALANCE_JOINT_COST = 3     # Joints required for balancing.
+OTHER_JOINT_COST = .3    # Other joints.
+
+# Construct the Qjoint matrix.
+Qjoint = np.eye(nv)
+Qjoint[root_dofs, root_dofs] *= 0  # Don't penalize free joint directly.
+Qjoint[balance_dofs, balance_dofs] *= BALANCE_JOINT_COST
+Qjoint[other_dofs, other_dofs] *= OTHER_JOINT_COST
+
+# Construct the Q matrix for position DoFs.
+Qpos = BALANCE_COST * Qbalance + Qjoint
+
+# No explicit penalty for velocities.
+Q = np.block([[Qpos, np.zeros((nv, nv))],
+              [np.zeros((nv, 2*nv))]])
+
+
+# Set the initial state and control.
+mujoco.mj_resetData(model, data)
+data.ctrl = ctrl0
+data.qpos = qpos0
+
+# Allocate the A and B matrices, compute them.
+A = np.zeros((2*nv, 2*nv))
+B = np.zeros((2*nv, nu))
+epsilon = 1e-6
+flg_centered = True
+mujoco.mjd_transitionFD(model, data, epsilon, flg_centered, A, B, None, None)
+
+# Solve discrete Riccati equation.
+P = scipy.linalg.solve_discrete_are(A, B, Q, R)
+
+# Compute the feedback gain matrix K.
+K = np.linalg.inv(R + B.T @ P @ B) @ B.T @ P @ A
+
+# Allocate position difference dq.
+dq = np.zeros(model.nv)
+
+input("sdf")
+
 with mujoco.viewer.launch_passive(model, data) as viewer:
     # Close the viewer automatically after 30 wall-seconds.
     start = time.time()
     while viewer.is_running() and time.time() - start < 30:
         step_start = time.time()
+#
+        # Get state difference dx.
+        mujoco.mj_differentiatePos(model, dq, 1, qpos0, data.qpos)
+        dx = np.hstack((dq, data.qvel)).T
 
-        data.qacc = 0  # Assert that there is no the acceleration.
-        mujoco.mj_inverse(model, data)
-        print(data.qfrc_inverse)
+        # LQR control law.
+        data.ctrl = ctrl0 - K @ dx
 
-        input("step")
         mujoco.mj_step(model, data)
-
-        # Example modification of a viewer option: toggle contact points every two seconds.
-        with viewer.lock():
-            viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_CONTACTPOINT] = int(
-                data.time % 2)
 
         # Pick up changes to the physics state, apply perturbations, update options from GUI.
         viewer.sync()
