2e7bff7bff775ccd348ba349a31cef13fe282d3b
Andrei Herdt <andrei.herdt@gmail.com>
2023-09-29 23:31:16 +0100

    wip: add orientation task

--- control/qp_manipulator.py
+++ control/qp_manipulator.py
@@ -38,6 +38,8 @@ mujoco.mj_comPos(model, data)
 
 # Get the Jacobian for the root body (torso) CoM.
 Je = np.zeros((3, nv))
+Jebt = np.zeros((3, nv))
+Jebr = np.zeros((3, nv))
 
 M = np.zeros((model.nv, model.nv))
 Minv = np.zeros((model.nv, model.nv))
@@ -45,11 +47,13 @@ Minv = np.zeros((model.nv, model.nv))
 nforce = 3
 A1 = np.zeros((3, 6+nforce))
 A2 = np.zeros((6, 6+nforce))
+A4 = np.zeros((3, 6+nforce))
 
 # Task weights
 W1 = 10*np.identity(3)
 W2 = 1*np.identity(6)
 W3 = .01*np.identity(6+nforce)
+W4 = 0*np.identity(3)
 
 # Constants
 x_c_d = data.subtree_com[0].copy()
@@ -81,7 +85,12 @@ C = None
 u = None
 l = None
 l_box = -np.ones(n) * 1.0e2
+l_box[8] = 100
 u_box = np.ones(n) * 1.0e2
+
+mujoco.mj_jacBody(model, data, Jebt, Jebr, ee_id)
+mujoco.mj_jacSubtreeCom(model, data, Je, ee_id)
+__import__('pdb').set_trace()
 
 sim_start = time.time()
 with mujoco.viewer.launch_passive(model, data) as viewer:
@@ -101,27 +110,41 @@ with mujoco.viewer.launch_passive(model, data) as viewer:
         M1 = M[:6,:6]
         h1 = h[:6]
         Minv = np.linalg.inv(M1)
-        mujoco.mj_jacSubtreeCom(model, data, Je, model.body('bracelet_link').id)
+        mujoco.mj_jacSubtreeCom(model, data, Je, ee_id)
+        mujoco.mj_jacBody(model, data, Jebt, Jebr, ee_id)
         J1 = Je[:,:6]
         J2 = np.eye(6,6)
+        J4 = Jebr[:,:6]
         A1[:,:6] = J1@Minv
         A1[:,6:] = J1@Minv@J1.T
         A2[:,:6] = J2@Minv
         A2[:,6:] = J2@Minv@J1.T
+        A4[:,:6] = J4@Minv
+        A4[:,6:] = J4@Minv@J1.T
         H1 = A1.T@W1@A1
         H2 = A2.T@W2@A2
-        H = H1 + H2 + W3[:9,:9]
+        H4 = A4.T@W4@A4
+        H = H1 + H2 + W3[:9,:9] + H4 
         Hpinv = np.linalg.pinv(H)
 
         r1 = (A1[:,:6]@h1+ddotx_c_d(x_c, dx_c))@W1@A1
         r2 = (A2[:,:6]@h1+ddotq_d(data.qpos[:6], data.qvel[:6]))@W2@A2
-        g = r1 + r2
+
+        quat_d_ee = np.array([ 0.631, 0.009,-0.019,-0.775])
+        angvel = np.zeros(3)
+        mujoco.mju_subQuat(angvel, data.body(ee_id).xquat, quat_d_ee)
+        r4 = (A4[:,:6]@h1+angvel)@W4@A4
+
+        g = r1 + r2 + r4
 
         qp.init(H, -g, A, b, C, l, u, l_box, u_box)
         qp.solve()
 
         tau_d = qp.results.x[:6]
         force = qp.results.x[6:9]
+
+        print(data.body(ee_id).xquat)
+        print(angvel)
 
         data.ctrl[:6] = tau_d
 
