3e7c828076f400d9e1bfb147dd4d41ada909a35e
Andrei Herdt <andrei.herdt@gmail.com>
2023-09-30 09:49:57 +0100

    strong oscillations

--- control/qp_manipulator.py
+++ control/qp_manipulator.py
@@ -25,10 +25,8 @@ data = mujoco.MjData(model)
 ee_id = model.body('bracelet_link').id
 ee_com = data.subtree_com[ee_id]
 
-
 nu = model.nu  # Alias for the number of actuators.
 nv = model.nv  # Shortcut for the number of DoFs.
-
 
 for i in range(nu):
     data.qpos[i] = 0.5
@@ -54,6 +52,7 @@ W1 = 10*np.identity(3)
 W2 = 1*np.identity(6)
 W3 = .01*np.identity(6+nforce)
 W4 = 0*np.identity(3)
+W4[2,2] = 0
 
 # Constants
 x_c_d = data.subtree_com[0].copy()
@@ -65,7 +64,7 @@ g = np.array([0, 0, 9.81])
 Kp_c = 1000
 Kd_c = 100
 Kp_q = 0
-Kd_q = 10
+Kd_q = 1
 
 def ddotx_c_d(p, v): 
     return -Kp_c * (p - x_c_d) - Kd_c * (v - np.zeros(3))
@@ -85,7 +84,7 @@ C = None
 u = None
 l = None
 l_box = -np.ones(n) * 1.0e2
-l_box[8] = 100
+# l_box[8] = 100
 u_box = np.ones(n) * 1.0e2
 
 mujoco.mj_jacBody(model, data, Jebt, Jebr, ee_id)
@@ -115,6 +114,7 @@ with mujoco.viewer.launch_passive(model, data) as viewer:
         J1 = Je[:,:6]
         J2 = np.eye(6,6)
         J4 = Jebr[:,:6]
+        # todo: double check J1.T
         A1[:,:6] = J1@Minv
         A1[:,6:] = J1@Minv@J1.T
         A2[:,:6] = J2@Minv
@@ -127,13 +127,13 @@ with mujoco.viewer.launch_passive(model, data) as viewer:
         H = H1 + H2 + W3[:9,:9] + H4 
         Hpinv = np.linalg.pinv(H)
 
-        r1 = (A1[:,:6]@h1+ddotx_c_d(x_c, dx_c))@W1@A1
-        r2 = (A2[:,:6]@h1+ddotq_d(data.qpos[:6], data.qvel[:6]))@W2@A2
+        r1 = (A1[:,:6]@h1 + ddotx_c_d(x_c, dx_c))@W1@A1
+        r2 = (A2[:,:6]@h1 + ddotq_d(data.qpos[:6], data.qvel[:6]))@W2@A2
 
         quat_d_ee = np.array([ 0.631, 0.009,-0.019,-0.775])
         angvel = np.zeros(3)
         mujoco.mju_subQuat(angvel, data.body(ee_id).xquat, quat_d_ee)
-        r4 = (A4[:,:6]@h1+angvel)@W4@A4
+        r4 = (A4[:,:6]@h1 + angvel)@W4@A4
 
         g = r1 + r2 + r4
 
