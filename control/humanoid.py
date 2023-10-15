ca16a5de5ada4bb01dad278ad21966f4099e5d8c
Andrei Herdt <andrei.herdt@gmail.com>
2023-10-15 13:06:21 +0100

    refactor: separate joint space objectives

--- control/humanoid.py
+++ control/humanoid.py
@@ -16,8 +16,8 @@ def create_gains_dict() -> Dict[str, float]:
     # Define gains
     Kp_c: float = 0
     Kd_c: float = 0
-    Kp_q: float = 100
-    Kd_q: float = 10
+    Kp_q: float = 1
+    Kd_q: float = .1
     Kp_r: float = 0
     Kd_r: float = 0
     
@@ -76,25 +76,21 @@ def create_weights(nv1: int, nu: int) -> dict:
     - Dictionary containing the weight arrays.
     """
     # Task weights
-    w2: float = 1
     W1: np.ndarray = 0 * np.identity(3)  # EE pos task
-    # todo
-    W2: np.ndarray = w2 * np.identity(nu)  # ddq2
-    W3: np.ndarray = 0.1 * np.identity(nu)  # tau
+    wq2: float = 1
+    q2: np.ndarray = wq2 * np.identity(nu)  # ddq1,ddq2
+    q: np.ndarray =  np.zeros((nv1 + nu,nv1 + nu))  # ddq1,ddq2
+    q[nv1:, nv1:] = 0 * np.identity(nu)  # ddq2
+    W3: np.ndarray = 1 * np.identity(nu)  # tau
     W4: np.ndarray = 0 * np.identity(3)  # EE orientation task
-    W2full: np.ndarray = w2 * np.identity(nv1 + nu)  # ddq1,ddq2
-    W2full[:nv1, :nv1] = 100 * np.identity(nv1)  # ddq1
-    W2full[6, 6] = 10000  # deck joint
-    W2full[:nv1, :nv1] = 0 * np.identity(nv1)  # ddq1
-    W2full[6, 6] = 0  # deck joint
 
     # Create and return the dictionary
     weights_dict = {
         'W1': W1,
-        'W2': W2,
+        'q2': q2,
+        'q': q,
         'W3': W3,
-        'W4': W4,
-        'W2full': W2full
+        'W4': W4
     }
     return weights_dict
 
@@ -132,7 +128,7 @@ def get_state(data,
 
     return state
 
-def compute_des_acc(t, ref, gains, state, data, nu, nv0):
+def compute_des_acc(t, ref, gains, state, data, nu, nv1):
     des_acc: Dict[str, np.ndarray] = {}
 
     r = 0.1
@@ -143,45 +139,51 @@ def compute_des_acc(t, ref, gains, state, data, nu, nv0):
     r = .0
     f = .0
     (x_d, v_d) = circular_motion(t, np.zeros(3), r, f)
-    des_acc['joints_full'] = ddotq_d_full(data.qpos, data.qvel, x_d, v_d, ref['p_d_root'], ref['R_d_root'], ref['q2_d'], np.zeros(nu+nv0), gains['Kp_q'], gains['Kd_q'])
+    des_acc['joints_full'] = ddotq_d_full(data.qpos, data.qvel, x_d, v_d, ref['p_d_root'], ref['R_d_root'], ref['q2_d'], np.zeros(nu+nv1), gains['Kp_q'], gains['Kd_q'])
     return des_acc
 
-def setupQPSparseFullFullJacTwoArms(M1, M2, h1, h2, C1, jacs, ee_ids, vmapu, weights, refs, nv0, nu, nforce, qp, qpproblem):
+def setupQPSparseFullFullJacTwoArms(M1, M2, h1, h2, C1, jacs, ee_ids, vmapu, weights, refs, nv1, nu, nforce, qp, qpproblem):
     ntau = nu
     # Assume arrangement
     # [tau,ddq_1, ddq_2, lambda] 
-    H = np.zeros((ntau+nu+nv0+nforce, ntau+nu+nv0+nforce))
-    g = np.zeros(ntau+nu+nv0+nforce)
+    H = np.zeros((ntau+nu+nv1+nforce, ntau+nu+nv1+nforce))
+    g = np.zeros(ntau+nu+nv1+nforce)
 
     J1 = jacs[ee_ids['torso']]['t']
-    J2 = np.eye(nu+nv0,nu+nv0)
+    J2 = np.eye(nu+nv1,nu+nv1)
     J4 = jacs[ee_ids['torso']]['r']
+    J5 = np.eye(nu,nu)
 
     W1 = weights['W1']
-    W2 = weights['W2full']
+    W2 = weights['q']
     W3 = weights['W3']
     W4 = weights['W4']
+    W5 = weights['q2']
 
     ref1 = refs['torso']
     ref2 = refs['joints_full']
+    ref5 = refs['joints']
 
     H[:nu, :nu] += W3 # tau
-    H[ntau:ntau+nv0+nu, ntau:ntau+nv0+nu] += J1.T@W1@J1 # ddq_2
-    H[ntau:ntau+nv0+nu, ntau:ntau+nv0+nu] += J2.T@W2@J2 # ddq_2
+    H[ntau:ntau+nv1+nu, ntau:ntau+nv1+nu] += J1.T@W1@J1 # ddq_2
+    H[ntau:ntau+nv1+nu, ntau:ntau+nv1+nu] += J2.T@W2@J2 # ddq
+    H[ntau+nv1:ntau+nv1+nu, ntau+nv1:ntau+nv1+nu] += J5.T@W5@J5 # ddq_2
 
     r1 = ref1@W1@J1
     r2 = ref2@W2@J2
+    r5 = ref5@W5@J5
 
-    g[ntau:ntau+nv0+nu] += r1  + r2 # ddq
+    g[ntau:ntau+nv1+nu] += r1 + r2# ddq
+    g[ntau+nv1:ntau+nv1+nu] += r5# ddq
 
-    qpproblem.A = np.zeros((nv0+nu, ntau+nu+nv0+nforce))
-    qpproblem.b = np.zeros(nv0+nu)
+    qpproblem.A = np.zeros((nv1+nu, ntau+nu+nv1+nforce))
+    qpproblem.b = np.zeros(nv1+nu)
 
-    qpproblem.A[nv0:nv0+nu,0:ntau] += -np.eye(ntau,ntau) # tau
-    qpproblem.A[0:nv0,ntau:ntau+nv0+nu] += M1 # ddq
-    qpproblem.A[nv0:nv0+nu,ntau:ntau+nv0+nu] += M2 # ddq
-    qpproblem.b[0:nv0] += -h1
-    qpproblem.b[nv0:nv0+nu] += -h2
-    qpproblem.A[0:nv0+nu,ntau+nv0+nu:] += -C1.T # lambda
+    qpproblem.A[nv1:nv1+nu,0:ntau] += -np.eye(ntau,ntau) # tau
+    qpproblem.A[0:nv1,ntau:ntau+nv1+nu] += M1 # ddq
+    qpproblem.A[nv1:nv1+nu,ntau:ntau+nv1+nu] += M2 # ddq
+    qpproblem.b[0:nv1] += -h1
+    qpproblem.b[nv1:nv1+nu] += -h2
+    qpproblem.A[0:nv1+nu,ntau+nv1+nu:] += -C1.T # lambda
 
     qp.init(H, -g, qpproblem.A, qpproblem.b, qpproblem.C, qpproblem.l, qpproblem.u, qpproblem.l_box, qpproblem.u_box)
