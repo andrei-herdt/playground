4a48b3c410a39ecc525b8a57419724e19ff1e8d3
Andrei Herdt <andrei.herdt@gmail.com>
2023-10-22 17:26:14 +0100

    Adapt dockerfile to build on x86

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-docker build . -t play_image:1.0 -f Dockerfile --cpuset-cpus 0-2
-docker build . -t play_image_dev:1.0 -f Dockerfile_dev --cpuset-cpus 0-2
+NCORES=$(cat /proc/cpuinfo | grep processor | wc -l)
+((--NCORES))
+docker build --build-arg "NCORE=${NCORES}" --cpuset-cpus "0-${NCORES}" . -t play_image:1.0 -f Dockerfile   \
+&& docker build --build-arg "NCORE=${NCORES}" --cpuset-cpus "0-${NCORES}" . -t play_image_dev:1.0 -f Dockerfile_dev
