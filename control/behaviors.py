918bad3648a4d886509372f8fb1769d4c13306ae
Andrei Herdt <andrei.herdt@gmail.com>
2023-11-16 09:33:36 +0000

    replace PosSequence through behaviour tree impl

--- control/behaviors.py
+++ control/behaviors.py
@@ -2,23 +2,78 @@ import numpy as np
 from typing import List
 
 
-class PosSequence:
-    def __init__(self, pos: List[np.ndarray]):
-        self.i: int = 0
-        self.positions: List[np.ndarray] = pos
+class BehaviorNode:
+    def pre_condition(self, *args, **kwargs):
+        return True
 
-    def update(self, pos: np.ndarray) -> np.ndarray:
-        # if (self.i == len(self.positions) - 1):
-        #     return self.positions[self.i]
+    def execute(self, *args, **kwargs):
+        if self.pre_condition(*args, **kwargs):
+            self._execute(*args, **kwargs)
+            return self._post_condition(*args, **kwargs)
+        else:
+            return False
 
-        dist = np.linalg.norm(self.positions[self.i] - pos)
+    def _execute(self, *args, **kwargs):
+        pass
+
+    def _post_condition(self, *args, **kwargs):
+        return True
+
+
+class MoveNode(BehaviorNode):
+    def __init__(self, pos_des: np.ndarray):
+        super().__init__()
+        self.pos_des = pos_des.copy()
+
+    def execute(self, pos: np.ndarray, ref: np.ndarray):
+        if self.pre_condition(pos, ref):
+            self._execute(ref)
+            return self._post_condition(pos)
+        else:
+            return False
+
+    def _execute(self, ref: np.ndarray):
+        ref[:] = self.pos_des.copy()
+
+    def _post_condition(self, pos: np.ndarray):
+        print("post_condition")
+        dist = np.linalg.norm(self.pos_des - pos)
         if dist < 0.015:
             print("dist: ", dist)
-            # Loop
-            if self.i == len(self.positions) - 1:
-                print("restarting loop")
-                self.i = 0
-            else:
-                print("incrementing sequence counter")
-                self.i = self.i + 1
-        return self.positions[self.i]
+            return True
+        else:
+            return False
+
+
+class SequenceNode(BehaviorNode):
+    def __init__(self, children):
+        super().__init__()
+        self.children: List[BehaviorNode] = children
+        self.child_id: int = 0
+        self.status = False
+
+    def _execute(self, *args, **kwargs) -> bool:
+        if self.child_id >= len(self.children):
+            self.child_id = 0
+            self.status = False
+            return self.status
+        print(self.status)
+        self.status = self.children[self.child_id].execute(*args, **kwargs)
+        print(self.status)
+        if self.status:
+            self.status = False
+            self.child_id = self.child_id + 1
+            print("incrementing")
+
+
+# TODO: Add test cases
+# sequence_node.execute(np.zeros(3), ref)
+# print("ref: ", ref)
+# sequence_node.execute(np.zeros(3), ref)
+# print("ref: ", ref)
+# sequence_node.execute(np.ones(3) / 3, ref)
+# print("ref: ", ref)
+# sequence_node.execute(np.ones(3) / 2, ref)
+# print("ref: ", ref)
+# sequence_node.execute(np.ones(3), ref)
+# print("ref: ", ref)
