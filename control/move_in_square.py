750956e363949f86823a5d45ecc512a43ac9fa31
Andrei Herdt <andrei.herdt@gmail.com>
2023-11-16 16:13:21 +0000

    wip: add behavior definition

---
+++ control/move_in_square.py
@@ -0,0 +1,13 @@
+# RT
+# positions[0][2] += 0.1
+# positions[0][1] -= 0.1
+
+# # LT
+# positions[1][2] += 0.1
+# positions[1][1] += 0.1
+# # LB
+# positions[2][2] -= 0.1
+# positions[2][1] += 0.1
+# # RB
+# positions[3][2] -= 0.1
+# positions[3][1] -= 0.1
