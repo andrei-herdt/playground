44f430abe088614d4816ab1b2d1bbc96c7b6a1e1
Andrei Herdt <andrei.herdt@gmail.com>
2023-11-19 06:51:52 +0000

    add policy simulation

---
+++ control/rl/simulate.py
@@ -0,0 +1,83 @@
+from brax.io import model
+from brax.training.agents.ppo.networks import make_inference_fn
+from brax.training.agents.ppo import networks as ppo_networks
+from brax.training.acme import running_statistics
+from brax import envs
+
+from environments import BarkourEnv, State
+
+import jax
+from jax import numpy as jp
+
+import functools
+
+import numpy as np
+
+import mujoco
+from mujoco import mj_step
+import mujoco.viewer
+import time
+
+
+# load params.
+model_path = "/workdir/mjx_brax_quadruped_policy"
+params = model.load_params(model_path)
+
+normalize = running_statistics.normalize
+make_networks_factory = functools.partial(
+    ppo_networks.make_ppo_networks, policy_hidden_layer_sizes=(128, 128, 128, 128)
+)
+ppo_network = make_networks_factory(465, 12, preprocess_observations_fn=normalize)
+make_inference_fn = ppo_networks.make_inference_fn(ppo_network)
+inference_fn = make_inference_fn(params)
+jit_inference_fn = jax.jit(inference_fn)
+
+# visualize policy
+#
+#
+envs.register_environment("barkour", BarkourEnv)
+env_name = "barkour"
+eval_env = envs.get_environment(env_name)
+
+jit_reset = jax.jit(eval_env.reset)
+jit_step = jax.jit(eval_env.step)
+
+model = eval_env.model
+data = mujoco.MjData(model)
+
+# @markdown Commands **only used for Barkour Env**:
+x_vel = 1.0  # @param {type: "number"}
+y_vel = 0.0  # @param {type: "number"}
+ang_vel = -0.5  # @param {type: "number"}
+
+the_command = jp.array([x_vel, y_vel, ang_vel])
+
+# initialize the state
+rng = jax.random.PRNGKey(0)
+state = jit_reset(rng)
+state.info["command"] = the_command
+rollout = [state]
+
+# grab a trajectory
+n_steps = 500
+render_every = 1
+
+with mujoco.viewer.launch_passive(
+    model, data, show_left_ui=False, show_right_ui=False
+) as viewer:
+    start = time.time()
+    while viewer.is_running():
+        step_start = time.time()
+
+        act_rng, rng = jax.random.split(rng)
+        ctrl, _ = jit_inference_fn(state.obs, act_rng)
+        data.ctrl = ctrl
+
+        mj_step(model, data)
+
+        viewer.sync()
+
+        # Rudimentary time keeping, will drift relative to wall clock.
+        dt = model.opt.timestep - (time.time() - step_start)
+        if dt > 0:
+            time.sleep(dt)
