a24ebba57731364b28f399c372ea899b0fde064a
Andrei Herdt <andrei.herdt@gmail.com>
2024-01-20 14:55:47 +0000

    extract cuda dockerfile

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -2,5 +2,18 @@
 
 NCORES=$(cat /proc/cpuinfo | grep processor | wc -l)
 ((--NCORES))
-docker build --build-arg "NCORE=${NCORES}" --cpuset-cpus "0-${NCORES}" . -t play_image:1.0 -f Dockerfile   \
-&& docker build --build-arg "NCORE=${NCORES}" --cpuset-cpus "0-${NCORES}" . -t play_image_dev:1.0 -f Dockerfile_dev
+
+if [ -f /usr/bin/nvidia-smi ]; then
+	BASE_IMAGE=nvidia/opengl:1.0-glvnd-runtime-ubuntu22.04
+	echo "nvidia"
+else
+	BASE_IMAGE=ubuntu:22.04
+fi
+
+docker build --build-arg "NCORE=${NCORES}" --build-arg "BASE_IMAGE=${BASE_IMAGE}" --cpuset-cpus "0-${NCORES}" . -t play_image:1.0 -f Dockerfile
+
+if [ -f /usr/bin/nvidia-smi ]; then
+	docker build --build-arg "NCORE=${NCORES}" --build-arg "BASE_IMAGE=${BASE_IMAGE}" --cpuset-cpus "0-${NCORES}" . -t play_image:1.0 -f Dockerfile_cuda
+	echo "nvidia"
+fi
+docker build --build-arg "NCORE=${NCORES}" --cpuset-cpus "0-${NCORES}" . -t play_image_dev:1.0 -f Dockerfile_dev
