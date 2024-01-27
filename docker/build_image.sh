662fddaad8354b1ddafd316f88b6f49c2de83fe6
Andrei Herdt <andrei.herdt@gmail.com>
2024-01-27 17:02:49 +0000

    fixes to dockerfile

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -3,11 +3,10 @@
 NCORES=$(cat /proc/cpuinfo | grep processor | wc -l)
 ((--NCORES))
 
+BASE_IMAGE=ubuntu:22.04
 if [ -f /usr/bin/nvidia-smi ]; then
 	BASE_IMAGE=nvidia/opengl:1.0-glvnd-runtime-ubuntu22.04
 	echo "nvidia"
-else
-	BASE_IMAGE=ubuntu:22.04
 fi
 
 docker build --build-arg "NCORE=${NCORES}" --build-arg "BASE_IMAGE=${BASE_IMAGE}" --cpuset-cpus "0-${NCORES}" . -t play_image:1.0 -f Dockerfile
